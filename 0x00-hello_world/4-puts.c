9adacb8f62558abb87fd31a54f0bc645977d810b
Peter Adebanji <Petermankind72@gmail.com>
2022-03-11 05:33:46 -0800

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
-*main - main block
-*Return: 0
+* main - prints the phrase "Programming is like building a multilingual puzzle
+* Return: 0
 */
 int main(void)
 {
