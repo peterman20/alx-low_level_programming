baea5e3b8e2e32441bef3d1d5391766906c5bc53
Peter Adebanji <Petermankind72@gmail.com>
2022-03-16 04:21:50 -0700

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet 10x
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char alp;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (alp = 'a'; alp < 'z'; alp++)
+		{
+			_putchar(alp);
+		}
+	_putchar('\n');
+	}
+}
