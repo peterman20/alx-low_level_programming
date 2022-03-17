3b96152a0c77825661f3ac5eda7e38011c610fcb
Peter Adebanji <Petermankind72@gmail.com>
2022-03-17 03:19:34 -0700

    24 hours tiking

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,4 +3,20 @@
  * jack_bauer - prints every minute of the day of Jack Bauer
  * Return: Alwyas 0
  */
-void
+void jack_bauer(void)
+{
+	int hr, min;
+
+	for (hr = 0; hr < 24; hr++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
