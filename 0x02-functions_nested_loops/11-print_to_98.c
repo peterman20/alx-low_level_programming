8ba33c2f949776408d4cf29c1ace33952d8a7e4e
Peter Adebanji <Petermankind72@gmail.com>
2022-03-17 05:00:35 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,7 +7,7 @@
  */
 void print_to_98(int n)
 {
-	if (n <= 98)
+	if (n < 98)
 	{
 		for (n = n; n <= 97; n++)
 		{
@@ -19,7 +19,7 @@ void print_to_98(int n)
 		for (n = n; n > 98; n--)
 		{
 			printf("%d, ", n);
-			printf("98\n");
 		}
 	}
+	printf("98\n");
 }
