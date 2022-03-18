8f282416f452676741ab52a44e245195181fb870
Peter Adebanji <Petermankind72@gmail.com>
2022-03-18 05:19:36 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints first 98 fibonacci series
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, num1 = 0, num2 = 1, sum;
+
+	for (i = 0; i < 98; i++)
+	{
+		sum = num1 + num2;
+		printf("%d, ", sum);
+		num1 = num2;
+		numb2 = su;
+	}
+	printf("\n");
+
+	return (0);
+}
