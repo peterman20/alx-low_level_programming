1e83deedda236e2d47ce0def127085f9e0f885e7
Peter Adebanji <Petermankind72@gmail.com>
2022-03-18 03:35:23 -0700

    natural number of multiples of 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - List out all the multiples of 3 and 5
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+}
